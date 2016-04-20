6e6cde27bc758b541ffd5f71d97ad85517e82fb0
Tetyana <tkutsenko@gmail.com>
2016-04-20 15:31:18 -0700

    Added title and description to the form

--- pricify/pricify_app.py
+++ pricify/pricify_app.py
@@ -31,6 +31,8 @@ def index():
 @app.route('/upload', methods=['POST'])
 def upload():
     # Get the name of the uploaded file
+    title = request.form['product_title']
+    description = request.form['product_description']
     file = request.files['file']
     # Check if the file is one of the allowed types/extensions
     if file and allowed_file(file.filename):
@@ -52,72 +54,6 @@ def uploaded_file(filename):
     return send_from_directory(app.config['UPLOAD_FOLDER'], filename)
 
 
-
-# home page
-# @app.route('/')
-# def index():
-#     return '''
-#         <!DOCTYPE html>
-#         <html>
-#             <head>
-#                 <meta charset="utf-8">
-#                 <title>Pricify</title>
-#             </head>
-#           <body>
-#             <!-- page content -->
-#             <h1>See prices for your stuff.</h1>
-#             <p>
-#                 Model to define price for your garage sale.
-#             </p>
-#             <p>
-#                 <a href="/submit">Check price</a>
-#             </p>
-#           </body>
-#         </html>
-#         '''
-#
-# # Form page to submit text
-# @app.route('/submit')
-# def submission_page():
-#     return '''
-#         <h3>We try to predict price.</h3>
-#         <form action="/predict" method='POST' >
-#             <p>
-#                 <label>Input product title:</label>
-#                 <input type="text" name="product_title" />
-#             </p>
-#             <p>
-#                 <label>Input product description:</label>
-#                 <textarea rows="10" cols="45" name="product_description"></textarea></p>
-#             </p>
-#             <input type="submit" />
-#         </form>
-#         '''
-#
-# @app.route('/upload', methods=['GET', 'POST'])
-# def upload_file():
-#     if request.method == 'POST':
-#         file = request.files['file']
-#         if file and allowed_file(file.filename):
-#             filename = secure_filename(file.filename)
-#             file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
-#             return redirect(url_for('uploaded_file', filename=filename))
-#     return '''
-#     <!doctype html>
-#     <title>Upload new File</title>
-#     <h1>Upload new File</h1>
-#     <form action="" method=post enctype=multipart/form-data>
-#       <p><input type=file name=file>
-#          <input type=submit value=Upload>
-#     </form>
-#     '''
-# from flask import send_from_directory
-#
-# @app.route('/uploads/<filename>')
-# def uploaded_file(filename):
-#     return send_from_directory(app.config['UPLOAD_FOLDER'],
-#                                filename)
-#
 # # My word counter app
 # @app.route('/predict', methods=['POST'] )
 # def word_counter():
@@ -126,7 +62,6 @@ def uploaded_file(filename):
 #     proba = model.predict_proba(vectorizer.transform([word]))
 #     page = 'We think that word {0} is from {1} topic.'
 #     return page.format(word, topic[0])
-#
 #
 #
 # @app.route('/score', methods=['POST'])
