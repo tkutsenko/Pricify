76b5f8c342e13c56fe27ad4719b4709cf997f592
Tetyana <tkutsenko@gmail.com>
2016-04-26 15:25:19 -0700

    Added similar images to the template

--- pricify/pricify_app.py
+++ pricify/pricify_app.py
@@ -115,10 +115,7 @@ def predict_price():
     neighbors_lst = neighbors['neighbours'][0]
 
     similar_offers = data.filter_by(neighbors_lst, 'id')
-    print similar_offers
-
-    similar_offers['image_path'] = app.config['IMAGES_FOLDER'] + category_name + "/" + str(similar_offers['id']) + '.jpg'
-
+    similar_offers['image_path'] = similar_offers['id'].apply(lambda x: 'static' + 'category_name' + "/" + str(x) + '.jpg')
     return render_template('price.html', price = price, category = category, image = filename, offers = similar_offers)
 
 
